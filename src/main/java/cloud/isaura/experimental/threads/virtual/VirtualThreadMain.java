4f2d92f96ef4569c3db1aee618a624ca4cb3f10e
pasquale <zerinol11PG>
2022-11-23 15:35:14 +0100

    First commit

---
+++ src/main/java/cloud/isaura/experimental/threads/virtual/VirtualThreadMain.java
@@ -0,0 +1,11 @@
+package cloud.isaura.experimental.threads.virtual;
+
+public class VirtualThreadMain
+{
+    public static void main(String[] args) throws InterruptedException {
+        var vthread = Thread.ofVirtual().start(() -> {
+            System.out.println("Hello World from " + Thread.currentThread());
+        });
+        vthread.join();
+    }
+}
