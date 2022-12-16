501c08c5ee77ab789540247f04b4a2bfac091933
pasquale <zerinol13PG>
2022-12-16 11:37:14 +0100

    Fix for virtual threads

--- src/main/java/cloud/isaura/experimental/threads/virtual/VirtualThreadMain.java
+++ src/main/java/cloud/isaura/experimental/threads/virtual/VirtualThreadMain.java
@@ -2,7 +2,7 @@ package cloud.isaura.experimental.threads.virtual;
 
 public class VirtualThreadMain
 {
-    /*
+
     public static void main(String[] args) throws InterruptedException {
         var vthread = Thread.ofVirtual().start(() -> {
             System.out.println("Hello World from " + Thread.currentThread());
@@ -10,5 +10,5 @@ public class VirtualThreadMain
         vthread.join();
     }
 
-     */
+
 }
