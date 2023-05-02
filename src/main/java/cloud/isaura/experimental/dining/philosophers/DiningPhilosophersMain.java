7ed26f5030e4bf0fd4837b7a9b3287b7fe13adf2
pasquale <pasquale@hitech-sanita.it>
2023-05-02 22:17:45 +0200

    wip

--- src/main/java/cloud/isaura/experimental/dining/philosophers/DiningPhilosophersMain.java
+++ src/main/java/cloud/isaura/experimental/dining/philosophers/DiningPhilosophersMain.java
@@ -8,7 +8,7 @@ public class DiningPhilosophersMain
     public static void main(String[] args)
     {
 
-        DiningPhilosophersParams diningPhilosophersParams = new DiningPhilosophersParams(PhilosopherType.GREEK,80000,1000L,1000L,10);
+        DiningPhilosophersParams diningPhilosophersParams = new DiningPhilosophersParams(PhilosopherType.GERMAN,50000,1000L,1000L,10);
         DiningPhilosophers diningPhilosophers = new DiningPhilosophers();
         diningPhilosophers.agorazein(diningPhilosophersParams);
 
